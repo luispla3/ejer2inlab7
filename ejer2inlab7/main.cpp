705a9d6acfaec69ec594b913905edf071bf5abe3
Luis <luisplanellah@gmail.com>
2022-12-30 13:47:57 +0100

    Initial Commit

---
+++ ejer2inlab7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ejer2inlab7
+//
+//  Created by Sergio Planella Hernández on 30/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
